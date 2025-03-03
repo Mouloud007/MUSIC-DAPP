dfd6890a9c21035d0d8a3c323c9080bc8393803a
Mouloud <u2164302@uel.ac.uk>
2025-03-03 21:50:45 +0000

    Your commit message

--- music-dapp/music/utils/utils.js
+++
@@ -1,9 +0,0 @@
-export const convertTime = (time) => {
-  const date = new Date(time);
-  const formattedDate = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
-
-  return formattedDate;
-};
-
-export const shortenAddress = (address) =>
-  `${address?.slice(0, 5)}...${address?.slice(address.length - 4)}`;
